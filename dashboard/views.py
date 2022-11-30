48ef59326d5c92c796375bfd74bca067f68f4c36
kyeon06 <jkyeon06@gmail.com>
2022-11-30 17:05:13 +0900

    대시보드 화면에서 데이터 추가 레이아웃 만들고 그래프 출력하기

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import CountryData
+from .forms import CountryDataForm
 
-# Create your views here.
 def dashBoard(request):
-    return render(request, 'dashboard/dashboard.html')
+    if request.method == 'POST':
+        
+        form = CountryDataForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+  
+    else:
+        form = CountryDataForm()
+
+    country_data = CountryData.objects.all()
+    return render(request, 'dashboard/dashboard.html', {'dataset' : country_data, 'form': form})
