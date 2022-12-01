e6c4293a34105bf8a560f37cb644d57942b8b79e
kyeon06 <jkyeon06@gmail.com>
2022-12-01 14:01:36 +0900

    URLConf설정

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from community.views import indexPage
+from .views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
+    # 관리자 페이지 url
     path('admin/', admin.site.urls),
+    
     path('index/', indexPage, name='index'),
     path('community/', include('community.urls')),
     path('dashboard/', include('dashboard.urls')),
+    
+    
+    # django.contrib.auth.urls 장고 내장 인증 urls 활용
+    path('accounts/', include('django.contrib.auth.urls')),
+    # login 인증 path
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    # 계정 생성이 완료 됐다는 메세지를 보여줌
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done')
 ]
