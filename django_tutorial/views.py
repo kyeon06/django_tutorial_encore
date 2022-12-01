cfd98252c3958904a7be771ae3a38cf4f0e9b95d
kyeon06 <jkyeon06@gmail.com>
2022-12-01 14:25:32 +0900

    view 구현, 부트스트랩 아이콘 추가

--- django_tutorial/views.py
+++ django_tutorial/views.py
@@ -1,4 +1,13 @@
-class UserCreateView():
-    pass
-class UserCreateDoneTV():
-    pass
+from django.views.generic.base import TemplateView
+from django.views.generic import CreateView # 새로운 레코드 생성을 위한 폼 뷰 보임, 테이블 레코드 생성
+from django.urls import reverse_lazy
+from .forms import CreateUserForm
+
+class UserCreateView(CreateView):
+    form_class = CreateUserForm
+    template_name = 'registration/register.html'
+
+    success_url = reverse_lazy('register_done')
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registeration/register_done.html'
