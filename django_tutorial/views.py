bbcd08dbe8f4d2a845b9b0e9bb7874a79f55bc53
kyeon06 <jkyeon06@gmail.com>
2022-12-01 19:54:33 +0900

    비밀번호 변경 기능 구현

--- django_tutorial/views.py
+++ django_tutorial/views.py
@@ -8,6 +8,7 @@ class UserCreateView(CreateView):
     # forms.py에 정의 되어 있는 form 클래스
     form_class = CreateUserForm
     template_name = 'registration/register.html'
+    
     # 폼에 입력 된 내용에 에러가 없고, 테이블 레코드 생성이 완료된 후에 이동할 URL을 지정
     # url 패턴 전달인자, urls.py 모듈이 메모리 로딩된 후에 실행
     success_url = reverse_lazy('register_done')
