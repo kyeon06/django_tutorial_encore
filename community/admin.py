34d310a2d09dde78c689e354dfcd0e1ad9141b24
kyeon06 <jkyeon06@gmail.com>
2022-12-02 15:07:18 +0900

    admin article 글 작성 페이지 수정

--- community/admin.py
+++ community/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('제목', {'fields':['title']}),
+        ('내용', {'fields':['contents']}),
+        ('작성자 정보', {'fields':['name','url','email','owner']}),
+    ]
 
 # admin 페이지에 Article 데이터 모델 등록
 # 이렇게 하면 관리 페이지에서 데이터 CRUD를 할 수 있게 된다.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
