88e4ab1e4162cd88e28075a6e20f745b83e82d95
kyeon06 <jkyeon06@gmail.com>
2022-12-02 15:11:12 +0900

    admin Article 리스트 컬럼 지정 수정하기

--- community/admin.py
+++ community/admin.py
@@ -5,8 +5,11 @@ class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [
         ('제목', {'fields':['title']}),
         ('내용', {'fields':['contents']}),
-        ('작성자 정보', {'fields':['name','url','email','owner']}),
+        ('작성자 정보', {'fields':['name','url','email']}),
+        ('작성자 id', {'fields':['owner'], 'classes':['collapse']}),
     ]
+
+    list_display = ('pk','title','url','cdate')
 
 # admin 페이지에 Article 데이터 모델 등록
 # 이렇게 하면 관리 페이지에서 데이터 CRUD를 할 수 있게 된다.
